56dd9a26c29a5aac4827084a3ad1e929402b885f
zenboss <zenyes@gmail.com>
2018-04-09 18:36:54 +1200

    add gulp task to mark upload dir

--- gulpfile.js
+++ gulpfile.js
@@ -252,11 +252,11 @@ gulp.task('copyLocalEnvConfig', function () {
 
 // Make sure upload directory exists
 gulp.task('makeUploadsDir', function () {
-  // return fs.mkdir('modules/users/client/img/profile/uploads', function (err) {
-  //   if (err && err.code !== 'EEXIST') {
-  //     console.error(err);
-  //   }
-  // });
+  return fs.mkdir('/modules/articles/client/img/product/uploads/', function (err) {
+    if (err && err.code !== 'EEXIST') {
+      console.error(err);
+    }
+  });
 });
 
 // Angular template cache task
